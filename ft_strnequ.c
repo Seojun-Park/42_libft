223c9c5311af1cd3cfe683dec5de848dfdc9cf66
jin park <jinchul112@gmail.com>
2019-02-19 20:42:14 -0800

    libft

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: jinpark <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:14:26 by jinpark           #+#    #+#             */
-/*   Updated: 2019/02/19 19:02:48 by jinpark          ###   ########.fr       */
+/*   Updated: 2019/02/19 20:41:29 by jinpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	if(s1 == NULL || *s2 == NULL)
+	if(s1 == NULL || s2 == NULL)
 		return(0);
 	while(*s1 && *s2 && i < n)
 	{
